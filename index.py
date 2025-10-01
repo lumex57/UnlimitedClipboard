bb6368fffaf1b328ce657656d661d4345b0e3d43
lumex57 <133763368+lumex57@users.noreply.github.com>
2025-10-01 19:15:01 +0900

    alert button click test

--- index.py
+++ index.py
@@ -5,7 +5,15 @@ clipboard = []
 class UnlimitedClipboards(rumps.App):
     @rumps.clicked("Clear All Copies")
     def clearAllCopies(self, _):
-        print("delete buttonn")
+        response = rumps.alert(
+            title="Really?",
+            message="진짜 지울거임?",
+            ok="Comfirm", 
+            cancel="Cancel"
+        )
+
+        if response == 1:
+            print("clicked yes")
 
 if __name__ == "__main__":
     UnlimitedClipboards("UC").run()
